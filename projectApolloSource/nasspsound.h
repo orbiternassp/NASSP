688a2548780348c5c2465c894820fc2f6094c208
Mark Grant <movieman523@yahoo.com>
2005-02-13 02:43:09 +0000

    Added new sound definitions and moved some hard-coded strings into the file.

--- projectApolloSource/nasspsound.h
+++ projectApolloSource/nasspsound.h
@@ -22,6 +22,9 @@
 
   **************************** Revision History ****************************
   *	$Log$
+  *	Revision 1.1  2005/02/11 12:17:55  tschachim
+  *	Initial version
+  *	
   **************************************************************************/
 
 #define LAUNCH_COUNT_10_SOUND		"Count10.wav"
@@ -35,12 +38,12 @@
 #define SI_CUTOFF_SOUND				"si-cutoff.wav"
 #define A13_CRYO_STIR				"cryo-stir.wav"
 #define A13_PROBLEM					"we-have-a-problem.wav"
+#define A13_KRANZ					"kranz.wav"
 #define MASTERALARM_SOUND			"alarm.wav"
 #define PROBE_SOUND					"probe.wav"
 #define CMCABIN_SOUND				"cabin.wav"
 #define BUTTON_SOUND				"button.wav"
 #define GUARD_SOUND					"guard.wav"
-#define VOX_SOUND					"vox.wav"
 #define VOX_SOUND					"vox.wav"
 #define LAUNCH_SOUND				"saturn-launch.wav"
 #define TOWERJET_SOUND				"TowerJet.wav"
@@ -53,3 +56,12 @@
 #define POST_TOWER_JET_SOUND		"post-tower-jet.wav"
 #define WATERLOOP_SOUND				"waterloop.wav"
 #define LUNARASCENT_SOUND			"lunar-ascent.wav"
+#define FLAG_SPEECH					"flag-speech.wav"
+#define LEVA_SOUND					"LEVA.wav"
+#define SM_SEP_SOUND				"sm-sep.wav"
+#define A13_SM_SEP_SOUND			"sm-sep-exploded.wav"
+#define DEFAULT_SM_SEP_SOUND		"SMJetison.wav"
+#define SEPARATION_SOUND			"Separation.wav"
+#define SPLASH_SOUND				"Splash.wav"
+
+#define SOUND_DIRECTORY				"ProjectApollo"
