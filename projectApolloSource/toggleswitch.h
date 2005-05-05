09dddd9c5632ea89645f2c834b7768ebc2204588
Tschachim <joachim@synapplix.de>
2005-05-05 21:36:41 +0000

    Introduced PanelSwitchItem and IndicatorSwitch

--- projectApolloSource/toggleswitch.h
+++ projectApolloSource/toggleswitch.h
@@ -22,6 +22,11 @@
 
   **************************** Revision History ****************************
   *	$Log$
+  *	Revision 1.5  2005/04/22 14:05:15  tschachim
+  *	Init functions changed
+  *	SwitchListener introduced
+  *	Some defines
+  *	
   *	Revision 1.4  2005/04/01 15:38:34  tschachim
   *	Introduced RotationalSwitch
   *	
@@ -46,7 +51,21 @@
 
 class SwitchRow;
 
-class ToggleSwitch {
+class PanelSwitchItem {
+
+public:
+	void SetNext(PanelSwitchItem *s) { next = s; };
+	PanelSwitchItem *GetNext() { return next; };
+
+	virtual bool CheckMouseClick(int event, int mx, int my) = 0;
+	virtual void DrawSwitch(SURFHANDLE DrawSurface) = 0;
+
+protected:
+	PanelSwitchItem *next;
+
+};
+
+class ToggleSwitch: public PanelSwitchItem {
 
 public:
 	ToggleSwitch();
@@ -57,8 +76,6 @@ public:
 	void SetSize(int w, int h) { width = w; height = h; };
 	void SetPosition(int xp, int yp) { x = xp; y = yp; };
 	void SetState(bool s) { state = s; };
-	void SetNext(ToggleSwitch *s) { next = s; };
-	ToggleSwitch *GetNext() { return next; };
 	int GetState() { return state; };
 	void SetActive(bool s);
 	void SetVisible(bool v) {visible = v; };
@@ -106,7 +123,6 @@ protected:
 	VESSEL *OurVessel;
 	Sound Sclick;
 
-	ToggleSwitch *next;
 	SwitchRow *switchRow;
 };
 
@@ -236,7 +252,7 @@ protected:
 	RotationalSwitchPosition* next;
 };
 
-class RotationalSwitch {
+class RotationalSwitch: public PanelSwitchItem {
 
 public:
 	RotationalSwitch();
@@ -244,8 +260,6 @@ public:
 
 	void Init(int xp, int yp, int w, int h, SURFHANDLE surf, SwitchRow &row);
 	void AddPosition(int value, double angle);
-	void SetNext(RotationalSwitch *s) { next = s; };
-	RotationalSwitch *GetNext() { return next; };
 	void DrawSwitch(SURFHANDLE drawSurface);
 	bool CheckMouseClick(int event, int mx, int my);
 	int operator=(const int b);
@@ -260,13 +274,34 @@ protected:
 	RotationalSwitchPosition *positionList;
 	SURFHANDLE switchSurface;
 	Sound sclick;
-	RotationalSwitch *next;
 	RotationalSwitchBitmap bitmaps[RotationalSwitchBitmapCount];
 
 	double AngleDiff(double a1, double a2);
 	void DeletePositions();
 };
 
+class IndicatorSwitch: public PanelSwitchItem {
+
+public:
+	IndicatorSwitch();
+	virtual ~IndicatorSwitch();
+
+	void Init(int xp, int yp, int w, int h, SURFHANDLE surf, SwitchRow &row);
+	void DrawSwitch(SURFHANDLE drawSurface);
+	bool CheckMouseClick(int event, int mx, int my);
+	int operator=(const bool b) { state = b; return state; };
+	operator bool() {return state; };
+
+protected:
+	bool state;
+	double displayState;	//0: false, 1: moving, 2: moving, 3: true
+	int	x;
+	int y;
+	int width;
+	int height;
+	SURFHANDLE switchSurface;
+	SwitchRow *switchRow;
+};
 
 class PanelSwitches;
 
@@ -278,15 +313,13 @@ public:
 
 	bool CheckMouseClick(int id, int event, int mx, int my);
 	bool DrawRow(int id, SURFHANDLE DrawSurface);
-	void AddSwitch(ToggleSwitch *s) { s->SetNext(SwitchList); SwitchList = s; };
-	void AddRotationalSwitch(RotationalSwitch *s) { s->SetNext(RotationalSwitchList); RotationalSwitchList = s; };
+	void AddSwitch(PanelSwitchItem *s) { s->SetNext(SwitchList); SwitchList = s; };
 	void Init(int area, PanelSwitches &panel);
 	SwitchRow *GetNext() { return RowList; };
 	void SetNext(SwitchRow *s) { RowList = s; };
 
 protected:
-	ToggleSwitch *SwitchList;
-	RotationalSwitch *RotationalSwitchList;
+	PanelSwitchItem *SwitchList;
 	SwitchRow *RowList;
 	int PanelArea;
 	PanelSwitches *panelSwitches;
@@ -294,12 +327,14 @@ protected:
 	friend class ToggleSwitch;
 	friend class ThreePosSwitch;
 	friend class RotationalSwitch;
+	friend class IndicatorSwitch;
 };
 
-class SwitchListener {
+class PanelSwitchListener {
 
 public:
-	virtual void SwitchToggled(ToggleSwitch *s) = 0;
+	virtual void PanelSwitchToggled(ToggleSwitch *s) = 0;
+	virtual void PanelIndicatorSwitchStateRequested(IndicatorSwitch *s) = 0;
 };
 
 class PanelSwitches {
@@ -309,16 +344,17 @@ public:
 	bool CheckMouseClick(int id, int event, int mx, int my);
 	bool DrawRow(int id, SURFHANDLE DrawSurface);
 	void AddRow(SwitchRow *s) { s->SetNext(RowList); RowList = s; };
-	void Init(int id, VESSEL *v, SoundLib *s, SwitchListener *l) { PanelID = id; RowList = 0; vessel = v; soundlib = s; listener = l; };
+	void Init(int id, VESSEL *v, SoundLib *s, PanelSwitchListener *l) { PanelID = id; RowList = 0; vessel = v; soundlib = s; listener = l; };
 
 protected:
 	VESSEL *vessel;
 	SoundLib *soundlib;
-	SwitchListener *listener;
+	PanelSwitchListener *listener;
 	int	PanelID;
 	SwitchRow *RowList;
 
 	friend class ToggleSwitch;
 	friend class ThreePosSwitch;
 	friend class RotationalSwitch;
+	friend class IndicatorSwitch;
 };
