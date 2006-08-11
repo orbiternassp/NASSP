a270949ab864b30eba277034748a55d33dd76716
Mark Grant <movieman523@yahoo.com>
2006-08-11 18:44:56 +0000

    Beginnings of SECS implementation.

--- projectApolloSource/secs.h
+++ projectApolloSource/secs.h
@@ -22,6 +22,9 @@
 
   **************************** Revision History ****************************
   *	$Log$
+  *	Revision 1.3  2006/07/01 23:49:13  movieman523
+  *	Updated more documentation.
+  *	
   *	Revision 1.2  2006/06/25 21:19:45  movieman523
   *	Lots of Doxygen updates.
   *	
@@ -33,21 +36,41 @@
 #if !defined(_PA_SECS_H)
 #define _PA_SECS_H
 
+class Saturn;
+
 ///
 /// This class simulates the Sequential Events Control System in the CM.
 /// \ingroup InternalSystems
 /// \brief SECS simulation.
 ///
-
 class SECS : public e_object {
 
 public:
 	SECS();
 	virtual ~SECS();
 
+	void ControlVessel(Saturn *v);
+
 	void Timestep(double simt, double simdt);
 	bool IsPowered();
 
+	void LoadState(FILEHANDLE scn);
+	void SaveState(FILEHANDLE scn);
+
+protected:
+	int State;
+
+	double NextMissionEventTime;
+	double LastMissionEventTime;
+
+	Saturn *OurVessel;
 };
+
+//
+// Strings for state saving.
+//
+
+#define SECS_START_STRING		"SECS_BEGIN"
+#define SECS_END_STRING			"SECS_END"
 
 #endif // _PA_SECS_H
