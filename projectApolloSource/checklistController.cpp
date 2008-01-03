ef151ec516f9de13a9aca08a430493ded2c18ff2
Brian Engelhardt <brettelswin@hotmail.com>
2008-01-03 07:12:11 +0000

    Minor bugfixes, some internal structure changes.  Solved not-compiling, not-launching bug.

--- projectApolloSource/checklistController.cpp
+++ projectApolloSource/checklistController.cpp
@@ -47,7 +47,21 @@ ChecklistGroup ChecklistController::*getChecklistList()
 }
 bool ChecklistController::failChecklistItem(ChecklistItem* input)
 {
-	return false;
+	input->failed = true;
+	if (input->failEvent.group != -1)
+	{
+		active->sequence++;
+		action.push_front(*active);
+		active = new ChecklistContainer(input->failEvent);
+	}
+	else
+	{
+		active->sequence++;
+	}
+	input->group = -1;
+	input->index = 0;
+	getChecklistItem(input);
+	return true;
 }
 bool ChecklistController::completeChecklistItem(ChecklistItem* input)
 {
@@ -61,8 +75,10 @@ void ChecklistController::init()
 {
 	complete = false;
 	action = deque<ChecklistContainer>();
+	sat = NULL;
 	active = 0;
-	groups = list<ChecklistGroup>();
+	groups = set<ChecklistGroup,ChecklistGroupcompare>();
+	lastMissionTime = 0;
 	//Todo: Initial loading of complete list to groups.
 	//Todo: Initial loading of proper group to active.
 	//Todo: figure out how to save state... potentially spit out the queue in fifo order
@@ -77,4 +93,29 @@ void ChecklistController::init(Saturn *saturn)
 void ChecklistController::LinkCraft(Saturn *saturn)
 {
 	init(saturn);
+}
+void ChecklistController::clbkTimestep(double missionTime)
+{
+	lastMissionTime = missionTime;
+}
+ChecklistContainer::ChecklistContainer(ChecklistGroup groupin)
+{
+	group = groupin;
+	set = vector<ChecklistItem>();
+	sequence = set.begin();
+}
+ChecklistContainer::~ChecklistContainer()
+{
+}
+bool ChecklistGroupcompare::operator()(const ChecklistGroup& lhs, const ChecklistGroup& rhs)
+{
+	if(lhs.relativeEvent < rhs.relativeEvent)
+		return true;
+	if(lhs.relativeEvent > rhs.relativeEvent)
+		return false;
+	if(lhs.time < rhs.time)
+		return true;
+	if(lhs.time > rhs.time)
+		return false;
+	return false;
 }
