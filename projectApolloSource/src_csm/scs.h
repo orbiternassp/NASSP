578e7f5c2bbf51e4fb2390196efe9a5d82eef4d1
Swatch <swatch04@gmail.com>
2009-12-15 08:50:00 +0000

    Edited feature where EMS scroll can be output as a bitmap file for post-mission analysis and reference.  To use feature, just make sure GTASwitch is in the up-position and ungarded when Simulation is saved or exited.  EMS might need to be powered as well.   Currently this creates a file in Orbiter's root directory called EMSScroll.bmp.

--- projectApolloSource/src_csm/scs.h
+++ projectApolloSource/src_csm/scs.h
@@ -23,6 +23,9 @@
 
   **************************** Revision History ****************************
   *	$Log$
+  *	Revision 1.4  2009/12/07 08:34:36  jasonims
+  *	Added feature where EMS scroll can be output as a bitmap file for post-mission analysis and reference.  To use feature, just make sure GTASwitch is in the up-position and ungarded when Simulation is saved or exited.  EMS might need to be powered as well.   Currently this creates a file in Orbiter's root directory called Scroll.bmp.
+  *	
   *	Revision 1.3  2009/09/17 17:48:41  tschachim
   *	DSKY support and enhancements of ChecklistMFD / ChecklistController
   *	
@@ -367,7 +370,6 @@ public:
 	bool IsOff();
 	bool IsdVMode();
 	bool WriteScrollToFile();
-	bool WriteScrollToFile(SURFHANDLE surf);
 	void SaveState(FILEHANDLE scn);                                // SaveState callback
 	void LoadState(FILEHANDLE scn);                                // LoadState callback
 	
