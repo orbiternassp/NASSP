5d20cb483ec149694102c54b34b95135c7519c00
Tschachim <joachim@synapplix.de>
2012-10-26 18:48:18 +0000

    comment

--- projectApolloScript/ProjectApollo/dsky_lamptest.lua
+++ projectApolloScript/ProjectApollo/dsky_lamptest.lua
@@ -1,3 +1,6 @@
+-- Sample script doing V35 "DSKY lamp test"
+-- Type run("ProjectApollo/dsky_lamptest") to execute this script
+
 v = vessel.get_focusinterface()
 v:set_panelswitch("DskySwitchVerb",1)
 proc.wait_simdt(1)
@@ -6,7 +9,3 @@ proc.wait_simdt(1)
 v:set_panelswitch("DskySwitchFive",1)
 proc.wait_simdt(1)
 v:set_panelswitch("DskySwitchEnter",1)
-
-
-
-
