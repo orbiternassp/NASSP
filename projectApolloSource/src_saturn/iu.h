dfd7d24468e69cac057ab1ee1ec0dc532ee358df
Tschachim <joachim@synapplix.de>
2013-01-12 15:16:45 +0000

    Bugfix OrientAxis: Roll orientation as parameter instead of hardcoded heads down position.

--- projectApolloSource/src_saturn/iu.h
+++ projectApolloSource/src_saturn/iu.h
@@ -22,6 +22,9 @@
 
   **************************** Revision History ****************************
   *	$Log$
+  *	Revision 1.4  2010/02/22 14:23:31  tschachim
+  *	Apollo 7 S-IVB on orbit attitude control, venting and Saturn takeover mode for the VAGC.
+  *	
   *	Revision 1.3  2010/02/09 02:39:29  bluedragon8144
   *	Improved SIVB on orbit autopilot.  Now starts 20 seconds after cutoff.
   *	
@@ -409,7 +412,7 @@ protected:
 	void SIVBStop();
 	void SIVBBoiloff();
 	void TLIInhibit();
-	VECTOR3 OrientAxis(VECTOR3 &vec, int axis, int ref, double gainFactor);
+	VECTOR3 OrientAxis(VECTOR3 &vec, int axis, int ref, double gainFactor, VECTOR3 &vec2);
 
 	int TLIBurnState;
 	bool TLIBurnStart;
@@ -463,6 +466,7 @@ protected:
 	///
 	bool AttitudeHold;
 	VECTOR3 AttitudeToHold;
+	VECTOR3 AttitudeToHold2;
 };
 
 //
