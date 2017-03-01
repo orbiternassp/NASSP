0f4a24bd2ae88767d7fb5aa09a16372f2309cd08
indy91 <indy91@users.noreply.github.com>
2017-03-01 18:11:56 +0100

    RTCC MFD: Correct coordinate system transformation for TLI cutoff state vector; calculate TLI PAD with internal TLI solution if a LVDC++ targeting update has been done.

--- Orbitersdk/samples/ProjectApollo/src_rtccmfd/ApolloRTCCMFD.cpp
+++ Orbitersdk/samples/ProjectApollo/src_rtccmfd/ApolloRTCCMFD.cpp
@@ -2236,27 +2236,23 @@ void ApolloRTCCMFD::menuP30Upload()
 	{
 		if (G->g_Data.progVessel->use_lvdc)
 		{
-			double T_RP, tb5start;
-			OELEMENTS coe;
+			SevenParameterUpdate coe;
 			SaturnV* testves;
 
 			testves = (SaturnV*)G->g_Data.progVessel;
 
-			tb5start = testves->lvdc->TB5 - 17.0;
-			T_RP = G->P30TIG - tb5start - testves->lvdc->T_RG;
-
-			coe = OrbMech::coe_from_PACSS4(G->R_TLI, G->V_TLI, testves->lvdc->mu);
+			coe = G->rtcc->TLICutoffToLVDCParameters(G->R_TLI, G->V_TLI, G->P30TIG, testves->lvdc->TB5, testves->lvdc->mu, testves->lvdc->T_RG);
 
 			testves->lvdc->TU = true;
 			testves->lvdc->TU10 = false;
 
-			testves->lvdc->T_RP = T_RP;
-			testves->lvdc->C_3 = coe.h;
-			testves->lvdc->Inclination = coe.i;
+			testves->lvdc->T_RP = coe.T_RP;
+			testves->lvdc->C_3 = coe.C3;
+			testves->lvdc->Inclination = coe.Inclination;
 			testves->lvdc->e = coe.e;
-			testves->lvdc->alpha_D = coe.w;
-			testves->lvdc->f = coe.TA;
-			testves->lvdc->theta_N = coe.RA;
+			testves->lvdc->alpha_D = coe.alpha_D;
+			testves->lvdc->f = coe.f;
+			testves->lvdc->theta_N = coe.theta_N;
 
 		}
 	}
@@ -3420,41 +3416,48 @@ void ApolloRTCCMFD::menuCalcManPAD()
 
 			SaturnV *SatV = (SaturnV*)G->g_Data.progVessel;
 
-			LVDCTLIparam tliparam;
+			if (SatV->lvdc->TU)
+			{
+				G->TLI_PAD();
+			}
+			else
+			{
+				LVDCTLIparam tliparam;
 
-			tliparam.alpha_TS = SatV->lvdc->alpha_TS;
-			tliparam.Azimuth = SatV->lvdc->Azimuth;
-			tliparam.beta = SatV->lvdc->beta;
-			tliparam.cos_sigma = SatV->lvdc->cos_sigma;
-			tliparam.C_3 = SatV->lvdc->C_3;
-			tliparam.e_N = SatV->lvdc->e_N;
-			tliparam.f = SatV->lvdc->f;
-			tliparam.mu = SatV->lvdc->mu;
-			tliparam.MX_A = SatV->lvdc->MX_A;
-			tliparam.omega_E = SatV->lvdc->omega_E;
-			tliparam.R_N = SatV->lvdc->R_N;
-			tliparam.TargetVector = SatV->lvdc->TargetVector;
-			tliparam.TB5 = SatV->lvdc->TB5;
-			tliparam.theta_EO = SatV->lvdc->theta_EO;
-			tliparam.t_D = SatV->lvdc->t_D;
-			tliparam.T_L = SatV->lvdc->T_L;
-			tliparam.T_RG = SatV->lvdc->T_RG;
-			tliparam.T_ST = SatV->lvdc->T_ST;
+				tliparam.alpha_TS = SatV->lvdc->alpha_TS;
+				tliparam.Azimuth = SatV->lvdc->Azimuth;
+				tliparam.beta = SatV->lvdc->beta;
+				tliparam.cos_sigma = SatV->lvdc->cos_sigma;
+				tliparam.C_3 = SatV->lvdc->C_3;
+				tliparam.e_N = SatV->lvdc->e_N;
+				tliparam.f = SatV->lvdc->f;
+				tliparam.mu = SatV->lvdc->mu;
+				tliparam.MX_A = SatV->lvdc->MX_A;
+				tliparam.omega_E = SatV->lvdc->omega_E;
+				tliparam.R_N = SatV->lvdc->R_N;
+				tliparam.TargetVector = SatV->lvdc->TargetVector;
+				tliparam.TB5 = SatV->lvdc->TB5;
+				tliparam.theta_EO = SatV->lvdc->theta_EO;
+				tliparam.t_D = SatV->lvdc->t_D;
+				tliparam.T_L = SatV->lvdc->T_L;
+				tliparam.T_RG = SatV->lvdc->T_RG;
+				tliparam.T_ST = SatV->lvdc->T_ST;
 
-			G->rtcc->LVDCTLIPredict(tliparam, G->vessel, G->GETbase, G->dV_LVLH, G->P30TIG, G->R_TLI, G->V_TLI, T_TLI);
+				G->rtcc->LVDCTLIPredict(tliparam, G->vessel, G->GETbase, G->dV_LVLH, G->P30TIG, G->R_TLI, G->V_TLI, T_TLI);
 
-			opt.dV_LVLH = G->dV_LVLH;
-			opt.GETbase = G->GETbase;
-			opt.REFSMMAT = G->REFSMMAT;
-			opt.TIG = G->P30TIG;
-			opt.vessel = G->vessel;
-			opt.SeparationAttitude = _V(0.0*RAD, -120.0*RAD, 0.0);
-			opt.TLI = T_TLI;
-			opt.R_TLI = G->R_TLI;
-			opt.V_TLI = G->V_TLI;
-			opt.uselvdc = true;
+				opt.dV_LVLH = G->dV_LVLH;
+				opt.GETbase = G->GETbase;
+				opt.REFSMMAT = G->REFSMMAT;
+				opt.TIG = G->P30TIG;
+				opt.vessel = G->vessel;
+				opt.SeparationAttitude = _V(0.0*RAD, -120.0*RAD, 0.0);
+				opt.TLI = T_TLI;
+				opt.R_TLI = G->R_TLI;
+				opt.V_TLI = G->V_TLI;
+				opt.uselvdc = true;
 
-			G->rtcc->TLI_PAD(&opt, G->tlipad);
+				G->rtcc->TLI_PAD(&opt, G->tlipad);
+			}
 		}
 		else
 		{
