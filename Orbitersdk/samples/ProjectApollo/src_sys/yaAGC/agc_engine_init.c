872c95e44e29a8d54623c501d0237e63b47ce5a4
indy91 <indy91@users.noreply.github.com>
2017-07-14 11:01:08 +0200

    yaAGC commit: restarts now enable interrupts

--- Orbitersdk/samples/ProjectApollo/src_sys/yaAGC/agc_engine_init.c
+++ Orbitersdk/samples/ProjectApollo/src_sys/yaAGC/agc_engine_init.c
@@ -67,6 +67,7 @@
 		03/27/17 MAS    Fixed a parity-related program loading bug and
                          added initialization of a new night watchman bit.
 		04/16/17 MAS    Added initialization of warning filter variables.
+		05/16/17 MAS    Enabled interrupts at startup.
 */
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1300 ) // Microsoft Visual Studio Version 2003 and higher
@@ -238,8 +239,7 @@ agc_engine_init (agc_t * State, const char *RomImage, const char *CoreDump,
   RetVal = 0;
   State->CycleCounter = 0;
   State->ExtraCode = 0;
-  // I've seen no indication so far of a reset value for interrupt-enable. 
-  State->AllowInterrupt = 0;
+  State->AllowInterrupt = 1; // The GOJAM sequence enables interrupts
   State->InterruptRequests[8] = 1;	// DOWNRUPT.
   //State->RegA16 = 0;
   State->PendFlag = 0;
