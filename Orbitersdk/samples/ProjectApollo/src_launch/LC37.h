b7d7c5b291b1653925821b756382d8610ff12cfe
indy91 <indy91@users.noreply.github.com>
2018-01-15 22:36:04 +0100

    Add LC-37 files

---
+++ Orbitersdk/samples/ProjectApollo/src_launch/LC37.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+  This file is part of Project Apollo - NASSP
+  Copyright 2018
+
+  LC37 vessel
+
+  Project Apollo is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Project Apollo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Project Apollo; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  See http://nassp.sourceforge.net/license/ for more details.
+
+  **************************************************************************/
+
+#pragma once
+
+///
+/// \ingroup Ground
+///
+class LC37: public VESSEL2 {
+
+public:
+	LC37(OBJHANDLE hObj, int fmodel);
+	virtual ~LC37();
+
+	void clbkSetClassCaps(FILEHANDLE cfg);
+	void clbkPostCreation();
+	void clbkLoadStateEx(FILEHANDLE scn, void *status);
+	void clbkSaveState(FILEHANDLE scn);
+	int clbkConsumeDirectKey(char *kstate);
+	int clbkConsumeBufferedKey(DWORD key, bool down, char *kstate);
+	void clbkPreStep(double simt, double simdt, double mjd);
+	void clbkPostStep(double simt, double simdt, double mjd);
+
+protected:
+	bool firstTimestepDone;
+	bool abort;
+	double touchdownPointHeight;
+	char LVName[256];
+	SoundLib soundlib;
+	OBJHANDLE hLV;
+	int state;
+
+	PSTREAM_HANDLE liftoffStream[2];
+	double liftoffStreamLevel;
+
+	void DoFirstTimestep();
+	void SetTouchdownPointHeight(double height);
+	void DefineAnimations();
+};
