5d475f1dcd2da707209244d6a09c822e33349735
indy91 <indy91@users.noreply.github.com>
2018-02-19 15:50:26 +0100

    Remove Fuel Cell heater duplicate and modify heater power

--- Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Esystems.cpp
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Esystems.cpp
@@ -377,8 +377,9 @@ void FCell::UpdateFlow(double dt)
 		return;
 	}
 
-	//In-line heater power, moreover this prevents the fuel cells from stopping
-	power_load += 160.0;
+	//Small idle power load to prevent the fuel cells from stopping
+	if (power_load < 1.0)
+		power_load += 1.0;
 
 	//first we check the start_handle;
 	double thrust = 0.0;
