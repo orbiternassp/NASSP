2126f0919e935fc8307ad0650e81ba0dfe46c45f
rcflyinghokie <rcflyinghokie@gmail.com>
2018-03-12 12:56:00 -0400

    Add remaining ECS caution logic

--- Orbitersdk/samples/ProjectApollo/src_lm/lemsystems.cpp
+++ Orbitersdk/samples/ProjectApollo/src_lm/lemsystems.cpp
@@ -4188,10 +4188,11 @@ void LEM_CWEA::TimeStep(double simdt){
 	// Restoration of normal water separator speed
 	// Selection of #2 suit fan
 	LightStatus[0][7] = 0;
-	if (lem->ECS_CO2_SENSOR_CB.IsPowered() && lem->scera1.GetVoltage(5, 2) >= (7.6/6)) { LightStatus[0][7] = 1; }
+	if (lem->ECS_CO2_SENSOR_CB.IsPowered() && lem->scera1.GetVoltage(5, 2) >= (7.6/6)) { LightStatus[0][7] = 1; }	// CO2 Partial Pressure > 7.6mm
 	if (lem->scera2.GetSwitch(12, 2)->IsClosed()) { LightStatus[0][7] = 1; } //Coolant pump 1 failure
 	if (lem->GlycolRotary.GetState() == 2 && lem->scera2.GetVoltage(13, 3) > 2.5) { LightStatus[0][7] = 1; } //Coolant pump 2 failure
-	//if (lem->SuitFanRotary.GetState() == 2) { LightStatus[0][7] = 0; }
+	if (lem->SuitFanRotary.GetState() == 1 && lem->scera2.GetVoltage(3, 5) > 2.5) { LightStatus[0][7] = 1; } //Suit fan 1 failure
+	if (lem->scera1.GetVoltage(5, 3) < 1.1) { LightStatus[0][7] = 1; } //Water separator failure
 
 	// 6DS37 OXYGEN QUANTITY CAUTION
 	// On when:
