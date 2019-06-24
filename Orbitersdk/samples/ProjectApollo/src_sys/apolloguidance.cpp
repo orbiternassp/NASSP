9f2c0555fcfdd6a3e0c62e1d422fdb3ba4553bf6
indy91 <indy91@users.noreply.github.com>
2019-06-24 13:10:25 +0200

    ApolloGuidance: Don't save old ICHAN input channel data

--- Orbitersdk/samples/ProjectApollo/src_sys/apolloguidance.cpp
+++ Orbitersdk/samples/ProjectApollo/src_sys/apolloguidance.cpp
@@ -424,14 +424,6 @@ void ApolloGuidance::SaveState(FILEHANDLE scn)
 	// And non-zero I/O state.
 	//
 
-	for (i = 0; i < MAX_INPUT_CHANNELS; i++) {
-		val = GetInputChannel(i);
-		if (val != 0) {
-			sprintf(fname, "ICHAN%03d", i);
-			oapiWriteScenario_int (scn, fname, val);
-		}
-	}
-
 	for (i = 0; i < MAX_OUTPUT_CHANNELS; i++) {
 		val = GetOutputChannel(i);
 		if (val != 0) {
@@ -439,7 +431,6 @@ void ApolloGuidance::SaveState(FILEHANDLE scn)
 			oapiWriteScenario_int (scn, fname, val);
 		}
 	}
-
 
 	for (i = 0; i < NUM_CHANNELS; i++) {
 		val = vagc.InputChannel[i];
