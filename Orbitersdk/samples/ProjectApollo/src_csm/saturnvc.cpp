060f10c8f880fdac5c6328d399d2b863bd9ef5d5
TheGondos <thegondos@protonmail.com>
2023-01-07 00:07:01 +0100

    [Sketchpad]Workaround for crash in 3D VC with EMS scroll texture

--- Orbitersdk/samples/ProjectApollo/src_csm/saturnvc.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/saturnvc.cpp
@@ -546,7 +546,17 @@ void Saturn::InitVC()
 	srf[SRF_VC_CW_LIGHTS] = oapiLoadTexture("ProjectApollo/VC/cw_lights.dds");
 	srf[SRF_VC_LVENGLIGHTS] = oapiLoadTexture("ProjectApollo/VC/lv_eng.dds");
 	srf[SRF_VC_EVENT_TIMER_DIGITS] = oapiLoadTexture("ProjectApollo/VC/event_timer.dds");
-	srf[SRF_VC_EMS_SCROLL_LEO] = oapiLoadTexture("ProjectApollo/VC/EMSscroll_LEO.dds");
+#if 0
+	srf[SRF_VC_EMS_SCROLL_LEO] = oapiLoadTexture("ProjectApollo/VC/EMSscroll_LEO.dds", true);
+#else
+	// As a workaround for the dynamic flag of oapiLoadTexture being lost on the way,
+	// we create a texture with oapiCreateTextureSurface which will have the correct flags
+	// and then blit the original texture onto it
+	SURFHANDLE tmp = oapiLoadTexture("ProjectApollo/VC/EMSscroll_LEO.dds");
+	srf[SRF_VC_EMS_SCROLL_LEO] = oapiCreateTextureSurface(4096, 256);
+	oapiBlt(srf[SRF_VC_EMS_SCROLL_LEO], tmp, 0, 0, 0, 0, 4096, 256);
+	oapiReleaseTexture(tmp);
+#endif
 	srf[SRF_VC_EMS_SCROLL_BORDER] = oapiLoadTexture("ProjectApollo/VC/EMSscroll_border.dds");
 	srf[SRF_VC_EMS_SCROLL_BUG] = oapiLoadTexture("ProjectApollo/VC/EMSscroll_bug.dds");
 	srf[SRF_VC_EMS_LIGHTS] = oapiLoadTexture("ProjectApollo/VC/ems_lights.dds");
