9b9b8f7676707c34d0eebe6d5fb7bd9edffd971f
Matthew W Hume <mtthwhm@gmail.com>
2023-01-22 01:20:25 -0500

    Improve H2, N2, and O2 density model based on NIST data
    Nitrogen density model
    
    add van der waal b coefficient. fix switch statement
    
    fix H2 density
    
    adjust density; dont mess with bulk modulus for now
    
    fix tank temps and final pass on densities and bulk modulus https://nvlpubs.nist.gov/nistpubs/Legacy/MONO/nbsmonograph168.pdf https://www.govinfo.gov/content/pkg/GOVPUB-C13-14652edffab8a46ccc4b08555c4d37d5/pdf/GOVPUB-C13-14652edffab8a46ccc4b08555c4d37d5.pdf
    
    Improve liquid hydrogen density model
    
    fix supercritical fluid boiling problem
    
    improve Hydrogen density model at temperatures up to 1200K
    
    code cleanup
    
    fix O2 Critical pressure
    
    update Critical pressures based on Antione at T_crit

--- Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.h
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.h
@@ -39,18 +39,18 @@
 #define SUBSTANCE_He			8
 
 #define R_CONST					8314.4621	//(L*Pa)/(mol*K)
-								//		O2			H2			H20			N2			CO2			GLYCOL			Aerozine-50		N2O4			He
-const double MMASS			[MAX_SUB]=	{31.998,	2.01588,	18.01528,	28.0134,	44.01,		33.434432,		92.146,			92.01,			4.00260		};		//g/mol
-const double SPECIFICC_GAS	[MAX_SUB]=	{0.658,		10.183,		1.4108,		0.743,		0.6553,		3.625769,		0.48102,		4.6,			3.12		};		//J/g-K .. assume constant
-const double SPECIFICC_LIQ	[MAX_SUB]=	{1.1519,	9.668,		4.184,		1.7848,		0.858,		3.691041,		1.0724,			1.5525,			5.193		};		//J/g-K .. assume constant
-const double L_DENSITY		[MAX_SUB]=	{1141.0,	70.0,		1000.0,		807.0,		1014.0,		1038.5,			899.0,			1450.0,			0.164		};		//g/L @ 103kPa ..assume constant wrt. temp
-const double BULK_MOD		[MAX_SUB]=	{32e6,		24e6,		2.18e6,		32e6,		32e6,		2.55e6,			1.47397e6,		1.362e6,		10e6		};		//Pa .. assume constant and converted from m^3 to L
-const double CRITICAL_P		[MAX_SUB]=  {350115.0,	89631.0,	1523741.0,	234421.0,	508833.0,	3097574.75,		11692906.154,	10132500.0,		226968.0224 };		//Pa.. critical pressure
-const double CRITICAL_T		[MAX_SUB]=  {154.7,		33.2,		647.3,		126.2,		304.4,		256.9525,		607.15,			431.15,			5.19		};		//K.. critical temperature
-const double ANTIONE_A		[MAX_SUB]=	{9.3199656,	6.59723,	12.490095,	9.0020008,	12.0892,	8.32957,		13.7222,		14.47645,		4.41952 };			//Antione Equation A constant gives results in bar, must be converter to Pa	[1]
-const double ANTIONE_B		[MAX_SUB]=	{838.91339,	133.793,	4658.1375,	694.78356,	2353.762,	3158.1575,		5309.7973,		4256.07694,		18.65037 };			//Antione Equation B constant gives results in bar, must be converter to Pa	[2]
-const double ACENTRIC		[MAX_SUB]=  {0.022,		-0.216,		0.345,		0.040,		0.288,		0.416,			0.316,			0.0141345,			-0.390};		//[3] Acentric factor
-
+								//		O2					H2					H20					N2					CO2					GLYCOL					Aerozine-50				N2O4					He
+const double MMASS			[MAX_SUB]=	{31.998,			2.01588,			18.01528,			28.0134,			44.01,				33.434432,				92.146,					92.01,					4.00260				};//g/mol
+const double SPECIFICC_GAS	[MAX_SUB]=	{0.658,				10.183,				1.4108,				0.743,				0.6553,				3.625769,				0.48102,				4.6,					3.12				};//J/g-K .. assume constant
+const double SPECIFICC_LIQ	[MAX_SUB]=	{1.1519,			9.668,				4.184,				1.7848,				0.858,				3.691041,				1.0724,					1.5525,					5.193				};//J/g-K .. assume constant
+const double L_DENSITY		[MAX_SUB]=	{1141.0,			70.0,				1000.0,				807.0,				1014.0,				1038.5,					899.0,					1450.0,					0.164				};//g/L @ 103kPa ..assume constant wrt. temp
+const double BULK_MOD		[MAX_SUB]=	{1.172e+9,			50.13e6,			2.18e6,				1.02E9,				32e6,				2.55e6,					1.47397e6,				1.362e6,				10e6				};//Pa .. assume constant and converted from m^3 to L
+const double CRITICAL_P		[MAX_SUB]=  {4926063.722233855,	1303085.962148109,	19910602.43884709,	3300253.307024634,	7800213.020695794,	3097566.149152389,		14500299.44342058,		9998268.775903003,		228416.3259285132	};//Pa.. critical pressure
+const double CRITICAL_T		[MAX_SUB]=  {154.7,				33.2,				647.3,				126.2,				304.4,				256.9525,				607.15,					431.15,					5.19				};//K.. critical temperature
+const double ANTIONE_A		[MAX_SUB]=	{9.3199656,			6.59723,			12.490095,			9.0020008,			12.0892,			8.32957,				13.7222,				14.47645,				4.41952				};//Antione Equation A constant gives results in bar, must be converter to Pa	[1]
+const double ANTIONE_B		[MAX_SUB]=	{838.91339,			133.793,			4658.1375,			694.78356,			2353.762,			3158.1575,				5309.7973,				4256.07694,				18.65037			};//Antione Equation B constant gives results in bar, must be converter to Pa	[2]
+const double ACENTRIC		[MAX_SUB]=  {0.022,				-0.216,				0.345,				0.040,				0.288,				0.416,					0.316,					0.0141345,				-0.390				};//[3] Acentric factor
+const double VDW_B			[MAX_SUB] = {9.957E-4,			0.015805,			0,					0.0387,				0,					0,						0,						0,						0,					};// Van der Waals equation 'b' coefficient in l/g
 //source for [1-3] are https://onlinelibrary.wiley.com/doi/pdf/10.1002/9781118135341.app1 or where they differ from the source they were adjusted by M. Hume to better fit a wide range of temperatures.
 // [3] https://nvlpubs.nist.gov/nistpubs/Legacy/IR/nbsir86-3054.pdf
 
@@ -80,6 +80,7 @@ class h_substance
 	h_substance operator* (float);	//returns a subst block that is "Ratio" part of the main (ie. 0.5 will generate half of the block)
 	void operator-= (h_substance);  //substact this block from itself
 	double VAPENTH() const;
+	double GET_LIQUID_DENSITY(const int SUBSTANCE_TYPE, const double temperature) const;
 	double Condense(double dt);
 	double Boil(double dt);
 	double BoilAll();
