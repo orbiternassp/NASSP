c9a396682278c07338f274dd6a404f9c964f708b
Ryan <rcflyinghokie@gmail.com>
2023-03-01 16:25:19 -0700

    Increase time between LM and CM uplinks pre DOI

--- Orbitersdk/samples/ProjectApollo/src_launch/MCC_Mission_H1.cpp
+++ Orbitersdk/samples/ProjectApollo/src_launch/MCC_Mission_H1.cpp
@@ -314,7 +314,7 @@ void MCC::MissionSequence_H1()
 		UpdateMacro(UTP_PADONLY, PT_LMP22ACQPAD, rtcc->GETEval2(rtcc->calcParams.SEP + 5.0*60.0), 58, MST_H1_LUNAR_ORBIT_PRE_DOI_3);
 		break;
 	case MST_H1_LUNAR_ORBIT_PRE_DOI_3: //LM SV update to CSM P76 PADs
-		UpdateMacro(UTP_LGCUPLINKONLY, PT_NONE, SubStateTime > 1.0*60.0, 400, MST_H1_LUNAR_ORBIT_PRE_DOI_4);
+		UpdateMacro(UTP_LGCUPLINKONLY, PT_NONE, SubStateTime > 2.0*60.0, 3, MST_H1_LUNAR_ORBIT_PRE_DOI_4);
 		break;
 	case MST_H1_LUNAR_ORBIT_PRE_DOI_4: //CSM P76 PADs to DOI Evaluation
 		UpdateMacro(UTP_PADWITHCMCUPLINK, PT_AP11P76PAD, rtcc->GETEval2(rtcc->calcParams.PDI - 21.0*60.0), 76, MST_H1_LUNAR_ORBIT_PRE_PDI_1);
