db65e61c7812edc288f08a5a0658000697aee43e
Ryan <rcflyinghokie@gmail.com>
2024-02-19 10:01:07 -0700

    Initial code implementation

--- Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.cpp
+++ Orbitersdk/samples/ProjectApollo/src_sys/PanelSDK/Internals/Hsystems.cpp
@@ -1277,17 +1277,29 @@ void h_MixingPipe::Save(FILEHANDLE scn) {
 }
 
 
-h_crew::h_crew(char *i_name, int nr, h_Tank *i_src) {
+h_crew::h_crew(char *i_name, int nr, h_Tank *i_src, h_Tank *i_h2o) {
 	
 	strcpy(name, i_name);
 	max_stage = 99;
 	number = nr;
 	SRC = i_src;
+	H2O = i_h2o;
 }
 
 void h_crew::refresh(double dt) {
 
 	double oxygen = 0.00949 * number * dt; //grams of O2 (0.082 to 0.124 LB/Man Hour (37.19 to 56.25 g/Man Hour) per LM-8 Systems Handbook)	
+	double water = 0.0346494 * number * dt;//6.6 lb/day per crew
+
+	if (H2O) {
+		double h2oTemp = H2O->GetTemp();
+		therm_obj *t = H2O->GetThermalInterface();
+
+		H2O->space.composition[SUBSTANCE_H2O].mass -= water;
+		H2O->space.composition[SUBSTANCE_H2O].vapor_mass -= water;
+		H2O->space.composition[SUBSTANCE_H2O].SetTemp(h2oTemp);
+	}
+
 	if (SRC) {
 		double srcTemp = SRC->GetTemp();
 		therm_obj *t = SRC->GetThermalInterface();
