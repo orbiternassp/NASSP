0b81af9998de2c93cc3fc9a431a8b46afca05982
Ryan <rcflyinghokie@gmail.com>
2024-02-19 10:56:17 -0700

    Add back commented code just in case

--- Orbitersdk/samples/ProjectApollo/src_lm/lm_ecs.cpp
+++ Orbitersdk/samples/ProjectApollo/src_lm/lm_ecs.cpp
@@ -805,7 +805,17 @@ void LEMCabinRepressValve::SystemTimestep(double simdt)
 	else if (cabinRepressValveSwitch->GetState() == 1)
 	{
 		cabinRepressValve->flowMax = 396.0 / LBH;
+/*
+		bool repressinhibit = false;
 
+		//Both in EGRESS
+		if (pressRegulatorASwitch->GetState() == 0 && pressRegulatorBSwitch->GetState() == 0) repressinhibit = true;
+		//One in EGRESS, other in CLOSE
+		else if (pressRegulatorASwitch->GetState() == 0 && pressRegulatorBSwitch->GetState() == 3) repressinhibit = true;
+		else if (pressRegulatorASwitch->GetState() == 3 && pressRegulatorBSwitch->GetState() == 0) repressinhibit = true;
+
+		if (cabinRepressCB->IsPowered() && repressinhibit == false)
+*/
 		if (cabinRepressCB->IsPowered())
 		{
 			if (lem->CabinPressureSwitch.GetPressureSwitch() != 0 && cabinRepressValve->in->open == 0)
