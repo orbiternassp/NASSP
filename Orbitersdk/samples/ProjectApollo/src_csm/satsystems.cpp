78ce41d2948c37c17d044ae15bdda7f0fd53ae54
Ryan <rcflyinghokie@gmail.com>
2025-04-07 10:36:57 -0600

    Edit debug line to match bool

--- Orbitersdk/samples/ProjectApollo/src_csm/satsystems.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/satsystems.cpp
@@ -1170,7 +1170,7 @@ void Saturn::SystemsTimestep(double simt, double simdt, double mjd) {
 	double *DockProbeTemp = (double *)Panelsdk.GetPointerByString("HYDRAULIC:DOCKPROBE:TEMP");
 	int *DockProbeHX = (int *)Panelsdk.GetPointerByString("HYDRAULIC:DOCKPROBEINCABIN:PUMP");
 
-	sprintf(oapiDebugString(), "Volts: %.2f Temp: %.4f Installed: %d HX: %d", DockProbeTempSensor.Voltage(), KelvinToFahrenheit(*DockProbeTemp), dockingprobe.InCabin(), *DockProbeHX);
+	sprintf(oapiDebugString(), "Volts: %.2f Temp: %.4f In Cabin: %d HX: %d", DockProbeTempSensor.Voltage(), KelvinToFahrenheit(*DockProbeTemp), dockingprobe.InCabin(), *DockProbeHX);
 	//*/
 
 // GSE Cryo Debug Lines
