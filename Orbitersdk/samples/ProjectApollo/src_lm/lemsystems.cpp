15256c7b4325555749ed0378429cc08bd994211b
Ryan <rcflyinghokie@gmail.com>
2025-08-20 19:29:05 -0600

    Change to the bitmap values instead of displayed values

--- Orbitersdk/samples/ProjectApollo/src_lm/lemsystems.cpp
+++ Orbitersdk/samples/ProjectApollo/src_lm/lemsystems.cpp
@@ -2480,15 +2480,10 @@ void LEM::CreateMissionSpecificSystems()
 	}
 	EventTimerDisplay.SetReverseAtZero(pMission->IsLMEventTimerReversingAtZero());
 	SBandSteerable.AngleInit(pMission->GetLMNumber()); //Initializes S Band Antenna To Proper Closeout Angles
-	if (pMission->GetLMNumber() < 6) //Initializes S Band Antenna Knobs To Proper Closeout Angles
+	if (pMission->GetLMNumber() >= 6) //Initializes S Band Antenna Knobs To Proper Closeout Angles
 	{
-		Panel12AntPitchKnob.SetInitValue(255.0 * RAD);
-		Panel12AntYawKnob.SetInitValue(0.0 * RAD);
-	}
-	else
-	{
-		Panel12AntPitchKnob.SetInitValue(-75.0 * RAD);
-		Panel12AntYawKnob.SetInitValue(-12.0 * RAD);
+		Panel12AntPitchKnob.SetInitValue(0.0);
+		Panel12AntYawKnob.SetInitValue(5.0);
 	}
 }
 
