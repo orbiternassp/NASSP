caa69b91ad8be1bd3b738b984129871eff09e50e
Ryan <rcflyinghokie@gmail.com>
2026-01-21 14:23:43 -0700

    add missing initialization values

--- Orbitersdk/samples/ProjectApollo/src_csm/eps.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/eps.cpp
@@ -44,6 +44,13 @@ CryoPressureSwitch::CryoPressureSwitch()
 	PressureSwitch2 = false;
 	dcacontrol = NULL;
 	dcbcontrol = NULL;
+
+	tank1 = NULL;
+	tank2 = NULL;
+	heater1 = NULL;
+	heater2 = NULL;
+	fan1 = NULL;
+	fan2 = NULL;
 }
 
 CryoPressureSwitch::~CryoPressureSwitch() 
