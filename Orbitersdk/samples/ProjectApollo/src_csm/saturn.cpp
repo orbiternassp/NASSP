457be7cfbbebd6b93d1c475154b62cfc9fb0872a
Ryan <rcflyinghokie@gmail.com>
2026-05-05 10:56:19 -0600

    add panelsdk to integral lighting

--- Orbitersdk/samples/ProjectApollo/src_csm/saturn.cpp
+++ Orbitersdk/samples/ProjectApollo/src_csm/saturn.cpp
@@ -372,9 +372,9 @@ Saturn::Saturn(OBJHANDLE hObj, int fmodel) : ProjectApolloConnectorVessel (hObj,
 	mechanicalAccelerometer(inertialData),
 	cws(SMasterAlarm, Bclick, Panelsdk),
 	dockingprobe(0, SDockingCapture, SDockingLatch, SDockingExtend, SUndock, CrashBumpS, Panelsdk),
-	LeftIntegralLights(11.0), //LH Integral Power (watts)
-	RightIntegralLights(13.1), //RH Integral Power (watts)
-	LEBIntegralLights(7.1), //LEB Integral Power (watts)
+	LeftIntegralLights(Panelsdk, 11.0), //LH Integral Power (watts)
+	RightIntegralLights(Panelsdk, 13.1), //RH Integral Power (watts)
+	LEBIntegralLights(Panelsdk, 7.1), //LEB Integral Power (watts)
 	LeftNumericLights(Panelsdk),
 	LEBNumericLights(Panelsdk),
 	MissionTimerDisplay(Panelsdk),
